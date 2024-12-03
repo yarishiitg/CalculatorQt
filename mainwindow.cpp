40c1329acec70ba4b0b57af0300c29423c902c11
ykumar41 <ykumar4@lululemon.com>
2024-12-04 00:39:48 +0530

    designed the calculator app using qt designer
    attached button released event with callback functions/slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,39 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+      , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    connect(ui->pb0,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb1,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb2,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb3,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb4,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb5,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb6,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb7,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb8,SIGNAL(released()), this,SLOT(digit_pressed()));
+    connect(ui->pb9,SIGNAL(released()), this,SLOT(digit_pressed()));
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
+
+void MainWindow::digit_pressed() {
+    qDebug() << "Button pressed";
+    QPushButton *button = (QPushButton *) sender();
+    double labelNumber;
+    QString newLabel;
+    labelNumber = (ui->labelDisplay->text() + button->text()).toDouble();
+    newLabel = QString::number(labelNumber,'g',16);
+    ui->labelDisplay->setText(newLabel);
+}
+
+void MainWindow::on_pbDot_released()
+{
+    ui->labelDisplay->setText(ui->labelDisplay->text() + ".");
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -4,20 +4,26 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
+
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
+
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void digit_pressed();
+    void on_pbDot_released();
 };
 #endif // MAINWINDOW_H
